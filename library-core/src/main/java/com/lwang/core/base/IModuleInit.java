1c960b2746a4f6065c4fcb46d99c9486423d16df
lwang <lwang057@163.com>
2021-04-17 08:32:42 +0800

    添加library-core核心库、library-res资源库

---
+++ library-core/src/main/java/com/lwang/core/base/IModuleInit.java
@@ -0,0 +1,11 @@
+package com.lwang.core.base;
+
+/**
+ * @Author lwang
+ * @Date 2021/4/17 08:24
+ * @Description
+ */
+public class IModuleInit {
+
+
+}
---
+++ library-core/src/main/java/com/lwang/core/config/ModuleLifecycleConfig.java
@@ -0,0 +1,11 @@
+package com.lwang.core.config;
+
+/**
+ * @Author lwang
+ * @Date 2021/4/17 08:24
+ * @Description
+ */
+public class ModuleLifecycleConfig {
+
+
+}
